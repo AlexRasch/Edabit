4271f7c732a49b0963f9bc9029feff9ea56bb6c1
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-20 01:19:48 +0200

    Added test project and test for "return the sum of two  numbers"

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,8 +8,12 @@ namespace Tests
         }
 
         [Theory]
+        [TestCase(3, 2, 5)]
+        [TestCase(-3, 6, -9)]
+        [TestCase(7, 3, 10)]
         public void Return_The_Sum_Of_Two_Number(int x, int y, int shouldReturn)
         {
+            // https://edabit.com/challenge/xfRucdwGksiyjZq4K
             Assert.Equals(Sum(x, y), shouldReturn);
         }
     }
