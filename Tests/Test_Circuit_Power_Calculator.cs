64e41caecc79ca46afd57e97452b9fee061c4bd6
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-20 02:03:50 +0200

    added test for Circuit power calculator

---
+++ Tests/Test_Circuit_Power_Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class Test_Circuit_Power_Calculator
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Theory]
+        [TestCase(230, 10, 2300)]
+        [TestCase(110, 3, 330)]
+        [TestCase(480, 20, 9600)]
+        public void Circuit_Power_Calculator_Test(int voltage, int current, int shouldReturn)
+        {
+            // https://edabit.com/challenge/L2fwjYi9YixY8kJfK
+            Assert.That(Domain.Circuit_Power_Calculator.Addition(voltage, current), Is.EqualTo(shouldReturn));
+        }
+    }
+}
