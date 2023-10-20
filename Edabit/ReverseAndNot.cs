45df7f509b6266cfa88a26537dbde8d630c4fc7a
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-21 01:25:23 +0200

    completed reverseandnot

---
+++ Edabit/ReverseAndNot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class ReverseAndNot
+    {
+        public static string Go(int input)
+        {
+            // Reverse
+            char[] firstPart = input.ToString().ToCharArray();
+            Array.Reverse(firstPart);
+
+            // Keep as it is
+            char[] secPart = input.ToString().ToCharArray();
+
+            // Create output string
+            char[] output = new char[firstPart.Length * 2];
+            for (int i = 0; i < firstPart.Length; i++)
+            {
+                output[i] = firstPart[i];
+            }
+            for (int i = 0; i < secPart.Length; i++)
+            {
+                output[i + secPart.Length] = secPart[i];
+            }
+
+
+            return new string(output);
+        }
+    }
+}
