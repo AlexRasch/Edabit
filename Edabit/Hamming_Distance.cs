6978e323404899a36edac2e3be56d8eb17c81657
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-20 14:24:35 +0200

    Completed hamming distance

--- Edabit/Hamming_Distance.cs
+++ Edabit/Hamming_Distance.cs
@@ -10,9 +10,16 @@ namespace Domain
     {
         public static int HammingDistance(string input1, string input2)
         {
+            if (input1.Length != input2.Length)
+                throw new Exception("Input does not have equal length");
+
             int difference = 0;
 
-
+            for (int i = 0; i < input1.Length; i++)
+            {
+                if (input1[i] != input2[i])
+                    difference++;
+            }         
 
             return difference;
         }
