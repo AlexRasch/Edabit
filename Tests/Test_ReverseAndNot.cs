45897eb54a72d8dd8b38e14dfe4335e5bd3e60b2
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-21 00:55:29 +0200

    added test for ReverseAndNot

---
+++ Tests/Test_ReverseAndNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Domain;
+
+namespace Tests
+{
+    internal class Test_ReverseAndNot
+    {
+
+        /* 
+         * Write a function that takes an integer i and returns a string with the integer backwards followed by the original integer.
+         *  - i is a non-negative integer.
+         *  - https://edabit.com/challenge/YGhgctqPsKQxQQCFS
+         * 
+         */
+
+
+        [Theory]
+        [TestCase(123, "321123")]
+        [TestCase(123456789, "987654321123456789")]
+
+        public void ReverseAndNot_Test(int input1, string shouldReturn)
+        {
+            Assert.That(Domain.ReverseAndNot.Go(input1) , Is.EquivalentTo(shouldReturn));   
+        }
+    }
+}
