10e7ad3ced506ff50676c8aa5c0f0cbfe89e05b6
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-20 13:33:50 +0200

    Completed Multiply by length and refactored test

---
+++ Edabit/Multiply_by_Length.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class Multiply_by_Length
+    {
+        public static int[] MultiplyByLength(int[] input)
+        {
+            // Create a new array with length equal to input array
+            int[] output = new int[input.Length];
+            int multiplyByLength = input.Length;
+
+            // Multiply 
+            for (int i = 0; i < multiplyByLength; i++)
+            {
+                output[i] = input[i] * multiplyByLength;
+            }
+
+            return output;
+        }
+    }
+}
--- Tests/Test_Multiply_by_Length.cs
+++ Tests/Test_Multiply_by_Length.cs
@@ -25,24 +25,13 @@ namespace Tests
         }
 
         [Theory]
+        [TestCase(new int[] { 2, 6,4,9}, new int[] { 8, 24, 16, 36 })]
+        [TestCase(new int[] { 4, 1, 1 }, new int[] { 12, 3, 3 })]
+        [TestCase(new int[] { 1, 0, 3, 3, 7, 2, 1 }, new int[] { 7, 0, 21, 21, 49, 14, 7 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
         public void Multiply_by_Length_Test(int[] input, int[] shouldReturn)
         {
-            // Input
-            int[] i1 = { 2, 6, 4, 9 };
-            int[] i2 = { 4, 1, 1 };
-            int[] i3 = { 1, 0, 3, 3, 7, 2, 1 };
-            int[] i4 = { 0 };
-
-            // Expected output
-            int[] o1 = { 8, 24, 16, 36 };
-            int[] o2 = { 12, 3, 3 };
-            int[] o3 = { 7, 0, 21, 21, 49, 14, 7 };
-            int[] o4 = { 0 };
-
-            Assert.AreEqual(o1, Domain.Multiply_by_Length.MultiplyByLength(i1));
-            Assert.AreEqual(o2, Domain.Multiply_by_Length.MultiplyByLength(i2));
-            Assert.AreEqual(o3, Domain.Multiply_by_Length.MultiplyByLength(i3));
-            Assert.AreEqual(o4, Domain.Multiply_by_Length.MultiplyByLength(i4));
+            Assert.AreEqual(Domain.Multiply_by_Length.MultiplyByLength(input), shouldReturn);
         }
     }
 }
