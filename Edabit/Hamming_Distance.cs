ce98f810b69f79d690d83af948200f3687b5f33d
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-10-20 14:11:39 +0200

    added test for hamming distance

---
+++ Edabit/Hamming_Distance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class Hamming_Distance
+    {
+        public static int HammingDistance(string input1, string input2)
+        {
+            int difference = 0;
+
+
+
+            return difference;
+        }
+    }
+}
---
+++ Tests/Test_Hamming_Distance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class Test_Hamming_Distance
+    {
+        /*
+         *  Hamming distance is the number of characters that differ between two strings.
+         * 
+         *  - Both strings will have the same length.
+         *  
+         *  https://edabit.com/challenge/K49LXsoMmS6tXxP7R
+         */
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Theory]
+        [TestCase("abcde", "bcdef", 5)]
+        [TestCase("abcde", "abcde", 0)]
+        [TestCase("strong", "strung", 1)]
+        public void Hamming_Distance_Test(string input1, string input2, int shouldReturn)
+        {
+            // https://edabit.com/challenge/L2fwjYi9YixY8kJfK
+            Assert.That(Domain.Hamming_Distance.HammingDistance(input1, input2), Is.EqualTo(shouldReturn));
+        }
+    }
+}
